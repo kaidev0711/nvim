77c8be67146b9edcfeb3b18c8062d0782a66c379
kaidev0711 <kaiz.developers@gmail.com>
2023-07-18 21:26:42 +0700

    update

--- lua/kaiz/monokai_pro.lua
+++ lua/kaiz/monokai_pro.lua
@@ -5,7 +5,13 @@ return {
 	name = "monokai-pro",
 	config = function()
 		require("monokai-pro").setup({
-			background_clear = { "float_win" },
+			background_clear = {
+				"float_win",
+				"toggleterm",
+				"telescope",
+				"renamer",
+				"notify",
+			},
 		})
 		vim.cmd([[colorscheme monokai-pro]])
 	end,
